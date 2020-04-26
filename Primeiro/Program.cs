95540c467ad028656a4aeb69ae161ea104e7ef8b
viviane rocha <vividerocha@gmail.com>
2020-04-26 12:01:30 -0300

    Programa simples de cálculo de área de Triangulo feito em c#

---
+++ Primeiro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite as medidas do triângulo X :");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite as medidas do triângulo Y :");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaA = x.calculaArea();
+            double areaB = y.calculaArea();
+
+            Console.WriteLine("A área do Triangulo X é : " + areaA.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("A área do Triangulo Y é : " + areaB.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaA > areaB)
+            {
+                Console.WriteLine("O triângulo X é maior!");
+            }
+            else
+            {
+                Console.WriteLine("O triângulo Y é maior!");
+            }
+
+        }
+
+        
+    }
+}
